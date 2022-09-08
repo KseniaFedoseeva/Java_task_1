841fe96b83ad13407771b17ed410b29c9929ac92
KseniaFedoseeva <Ks1887@yandex.ru>
2022-09-08 10:09:49 +0500

    Задача 3

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,6 +12,22 @@ public class Main {
 
         double d = 59.8;
         byte b = 127;
+
+        //Задача 3
+
+        int[] myArray = {5, 6, 9, 6, 2};
+        int temp = myArray[0];
+        myArray[0] = myArray[myArray.length-1];
+        myArray[myArray.length-1] = temp;
+
+        //средний элемент
+        int average = myArray[myArray.length/2];
+        int sum = average+myArray[0];
+
+        System.out.println(sum);
+
+
+
 
 
 
