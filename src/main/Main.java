e62c55a5d2b4f7d11bcae0830d96435153444f37
KseniaFedoseeva <Ks1887@yandex.ru>
2022-09-08 08:44:24 +0500

    Задача 2

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,16 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
+        //Задача 1
         System.out.println("Hello World!");
-	// write your code here
+
+        //Задача 2
+
+        double d = 59.8;
+        byte b = 127;
+
+
+
+
     }
 }
